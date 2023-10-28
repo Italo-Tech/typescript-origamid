cf470fa2e7f1f9fb4c8a2858f8c69424924d112b
Italo <italo@appbee.online>
2023-10-28 01:23:21 -0300

    Annotation e Inference

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 "use strict";
-const total = 100;
+let produto = "Livro";
+let preco = 200;
+const carro = {
+    marca: "BMW",
+    portas: 4,
+};
+const barato = preco < 200 ? true : "produto caro";
+// As anotações serão necessárias quando lidamos com funções.
 function somar(a, b) {
     return a + b;
 }
-console.log(somar(10, 15));
+somar(5, 10);
+// somar("10", "10");
+const nintendo = {
+    nome: "Nintendo",
+    preco: "2000",
+};
+function transformarPreco(produto) {
+    produto.preco = "R$ " + produto.preco;
+    return produto;
+}
+const produtoNovo = transformarPreco(nintendo);
+console.log(produtoNovo);
+//Exercício 1 - Conserte a função com TypeScript
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
+//Exercício 2 - Conserte as funções com TypeScript
+const input = document.querySelector("input");
+const total = localStorage.getItem("total");
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
+}
+function calcularGanho(value) {
+    const p = document.querySelector("p");
+    if (p) {
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem("total", input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+if (input) {
+    input.addEventListener("keyup", totalMudou);
+}
