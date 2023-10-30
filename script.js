ac865a0c2514473e7ceff2806c4572d6413a61ed
Italo <italo@appbee.online>
2023-10-30 15:05:05 -0300

    Union e Types

--- script.js
+++ script.js
@@ -1,50 +1,33 @@
 "use strict";
-let produto = "Livro";
-let preco = 200;
-const carro = {
-    marca: "BMW",
-    portas: 4,
-};
-const barato = preco < 200 ? true : "produto caro";
-// As anotações serão necessárias quando lidamos com funções.
-function somar(a, b) {
-    return a + b;
-}
-somar(5, 10);
-// somar("10", "10");
-const nintendo = {
-    nome: "Nintendo",
-    preco: "2000",
-};
-function transformarPreco(produto) {
-    produto.preco = "R$ " + produto.preco;
-    return produto;
-}
-const produtoNovo = transformarPreco(nintendo);
-console.log(produtoNovo);
-//Exercício 1 - Conserte a função com TypeScript
-function normalizarTexto(texto) {
-    return texto.trim().toLowerCase();
-}
-//Exercício 2 - Conserte as funções com TypeScript
-const input = document.querySelector("input");
-const total = localStorage.getItem("total");
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
-function calcularGanho(value) {
-    const p = document.querySelector("p");
-    if (p) {
-        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+let total = 200;
+total = "300";
+function isNumber(value) {
+    if (typeof value === "number") {
+        return true;
+    }
+    else {
+        return "Não é número";
     }
 }
-function totalMudou() {
-    if (input) {
-        localStorage.setItem("total", input.value);
-        calcularGanho(Number(input.value));
+// Retorna HTMLButtonElement | null
+const button = document.querySelector("button");
+// Optional chaining
+// Executa click() se button for diferente de null/undefined
+button?.click();
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === "number") {
+        return value;
+    }
+    else if (typeof value === "string") {
+        return Number(value);
+    }
+    else {
+        throw "value deve ser um número ou uma string";
     }
 }
-if (input) {
-    input.addEventListener("keyup", totalMudou);
-}
+console.log(toNumber("50"));
--- script.ts
+++ script.ts
@@ -1,62 +1,34 @@
-let produto = "Livro";
-let preco = 200;
+let total: string | number = 200;
+total = "300";
 
-const carro = {
-  marca: "BMW",
-  portas: 4,
-};
-
-const barato = preco < 200 ? true : "produto caro";
-
-// As anotações serão necessárias quando lidamos com funções.
-function somar(a: number, b: number) {
-  return a + b;
-}
-
-somar(5, 10);
-// somar("10", "10");
-
-const nintendo = {
-  nome: "Nintendo",
-  preco: "2000",
-};
-
-function transformarPreco(produto: { nome: string; preco: string }) {
-  produto.preco = "R$ " + produto.preco;
-  return produto;
-}
-
-const produtoNovo = transformarPreco(nintendo);
-console.log(produtoNovo);
-
-//Exercício 1 - Conserte a função com TypeScript
-function normalizarTexto(texto: string) {
-  return texto.trim().toLowerCase();
-}
-
-//Exercício 2 - Conserte as funções com TypeScript
-const input = document.querySelector("input");
-
-const total = localStorage.getItem("total");
-if (input && total) {
-  input.value = total;
-  calcularGanho(Number(input.value));
-}
-
-function calcularGanho(value: number) {
-  const p = document.querySelector("p");
-  if (p) {
-    p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+function isNumber(value: string | number) {
+  if (typeof value === "number") {
+    return true;
+  } else {
+    return "Não é número";
   }
 }
 
-function totalMudou() {
-  if (input) {
-    localStorage.setItem("total", input.value);
-    calcularGanho(Number(input.value));
+// Retorna HTMLButtonElement | null
+const button = document.querySelector("button");
+// Optional chaining
+// Executa click() se button for diferente de null/undefined
+button?.click();
+
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+
+function toNumber(value: number | string) {
+  if (typeof value === "number") {
+    return value;
+  } else if (typeof value === "string") {
+    return Number(value);
+  } else {
+    throw "value deve ser um número ou uma string";
   }
 }
 
-if (input) {
-  input.addEventListener("keyup", totalMudou);
-}
+console.log(toNumber("50"));
