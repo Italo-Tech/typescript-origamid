749f9c4787c3a9e46f12ca0f19961cc68b534e76
Ítalo Henrique <italosantosdick@hotmail.com>
2023-10-31 20:11:36 -0300

    Any

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
 "use strict";
-// Uma array é definida com o tipo de dado(s) que ela possui, seguida por []
-const numeros = [10, 30, 40, 5, 3, 30];
-const valores = [10, "Taxas", 40, "Produto", 3, 30];
-function maiorQue10(data) {
-    return data.filter((n) => n > 10);
+// O any indica que o dado pode conter qualquer tipo de dado do TypeScript. Devemos evitar ao máximo o uso do any, pois o mesmo remove todas as seguranças e conveniências que o TS fornece.
+function normalizar(texto) {
+    return texto.trim("").toLowerCase();
 }
-console.log(maiorQue10(numeros));
-function filtrarValores(data) {
-    return data.filter((item) => typeof item === "number");
-}
-console.log(filtrarValores(valores));
-const dados = [
-    ["senhor dos aneis", 80],
-    ["a guerra dos tronos", 120],
-];
-async function fetchCursos() {
-    const response = await fetch("https://api.origamid.dev/json/cursos.json");
-    const data = await response.json();
-    console.log(data);
-    mostrarCursos(data);
-}
-fetchCursos();
+console.log(normalizar(" DeSign"));
 function mostrarCursos(cursos) {
     cursos.forEach((curso) => {
-        let color;
-        if (curso.nivel === "iniciante") {
-            color = "blue";
-        }
-        else if (curso.nivel === "avancado") {
-            color = "red";
-        }
         document.body.innerHTML += `
       <div>
-        <h2 style="color: ${color};">${curso.nome}</h2>
+        <h2>${curso.nome}</h2>
         <p>Horas: ${curso.horas}</p>
-        <p>Aulas: ${curso.aulas}</p>
-        <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
-        <p>Tags: ${curso.tags.join(", ")}</p>
-        <p>Aulas: ${curso.idAulas.join(" | ")}</p>
       </div>
     `;
     });
 }
+const dados = "o any gera problemas";
+mostrarCursos(dados);
