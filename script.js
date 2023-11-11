832f28b0b3d88e1bf538194a4926068d7d810e4d
Italo <italo@appbee.online>
2023-11-10 22:05:25 -0300

    Instanceof

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
 "use strict";
-// null é um tipo primitivo que representa a ausência de valor. É comum em funções do DOM que fazem uma busca, retornarem null quando não são bem sucedidas.
-const button = document.querySelector("button");
-const config = localStorage.getItem("config");
-if (button !== null) {
-    button.click();
+class Produto {
+    nome;
+    preco;
+    autor;
+    constructor(nome, preco, autor) {
+        this.nome = nome;
+        this.preco = preco;
+        this.autor = autor;
+    }
 }
-if (button) {
-    button.click();
+// const livro = new Produto("Game of Thrones", 200);
+// console.log(livro instanceof Produto);
+class Livro extends Produto {
+    nome;
+    preco;
+    constructor(nome, preco, autor) {
+        super(nome, preco, autor);
+        this.nome = nome;
+        this.preco = preco;
+    }
 }
-if (button)
-    button.click();
-button?.click();
-console.log(typeof null);
-// undefined representa variáveis/propriedades que foram instanciadas, porém, os seus valores ainda não foram definidos.
-let total;
-console.log(total); // undefined
-const data = {};
-console.log(data.nome);
-const livro = {};
-const jogo = {
-    nome: "Ragnarok",
-};
-if (jogo.nome) {
-    jogo.nome?.toLowerCase();
+const livro = new Livro("A vida de Pi", 150, "David Rokkins");
+console.log(livro);
+class Jogo extends Produto {
+    nome;
+    constructor(nome, preco, autor) {
+        super(nome, preco, autor);
+        this.nome = nome;
+        this.autor = autor;
+    }
 }
-livro.nome?.toLowerCase();
+const jogo = new Jogo("League of Legends", 200, "Ítalo");
+console.log(jogo);
+function buscarProduto(busca) {
+    if (busca === "O  hobbit") {
+        return new Livro("J. R. R. Tolkien", 50, "Steve Jobs");
+    }
+    if (busca === "Dota") {
+        return new Jogo("Dota", 400, "RPG");
+    }
+    else {
+        return null;
+    }
+}
+const produto = buscarProduto("Dota");
+if (produto instanceof Livro) {
+    console.log(produto.nome);
+}
+if (produto instanceof Jogo) {
+    console.log(produto.nome);
+}
+if (produto instanceof Produto) {
+    console.log(produto.nome);
+}
+// 1 - Selecione o link utilizando o método getElementById.
+// 2 - Substitua o href do link (HTMLAnchorElement) de http:// para https://.
+// <a id="origamid" href="http://www.origamid.com">Origamid</a>
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace("http://", "https://");
+}
+console.dir(link);
