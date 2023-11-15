f5c0f21fdbb1e5cdb2bd6045725462796264c751
Ítalo Henrique <italosantosdick@hotmail.com>
2023-11-15 01:36:10 -0300

    Generic 1 - Generics

--- script.js
+++ script.js
@@ -1,88 +1,32 @@
-// Eventos
-// const button = document.querySelector("button");
-// button?.addEventListener("pointerdown", handleClick);
-// function handleClick(event: MouseEvent) {
-//   console.log(event.pageX);
+"use strict";
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const frutas = ["Melancia", "Banana", "Pêra", "Maça", "Abacate", "Uva"];
+// Sem generic
+// function firstFive(data: string[] | number[]) {
+//   return data.slice(0, 5);
 // }
-// function handleScroll(event: Event) {
-//   console.log(event);
-// }
-// window.addEventListener("scroll", handleScroll);
-// Eventos e instanceof
-// // function ativarMenu(event: MouseEvent | TouchEvent | KeyboardEvent) {
-// function ativarMenu(event: Event) {
-//   if (event instanceof MouseEvent) {
-//     console.log(event);
-//   }
-//   if (event instanceof TouchEvent) {
-//     console.log(event.touches[0].pageX);
-//   }
-// }
-// document.documentElement.addEventListener("mousedown", ativarMenu);
-// document.documentElement.addEventListener("touchstart", ativarMenu);
-// document.documentElement.addEventListener("keydown", ativarMenu);
-// this
-// const button = document.querySelector("button");
-// button?.addEventListener("click", ativarMenu);
-// function ativarMenu(this: HTMLButtonElement, event: MouseEvent) {
-//   console.log(this);
-// }
-// const button = document.querySelector("button");
-// button?.addEventListener("click", ativarMenu);
-// function ativarMenu(event: MouseEvent) {
-//   const elemento = event.currentTarget;
-//   if (elemento instanceof HTMLElement) {
-//     console.log(elemento.innerText);
-//   }
-// }
-// Utilizando a estrutura HTML/CSS abaixo, crie o script que irá fazer o botão mobile funcionar (ativar/desativar a navegação).
-// Estado dos elementos
-// menu inativo:
-// class="" em nav
-// aria-expanded="false" em button
-// aria-label="Abrir Menu" em button
-// menu ativo:
-// class="active" em nav
-// aria-expanded="true" em button
-// aria-label="Fechar Menu" em button
-const navClass = document.getElementById("nav");
-const btnMobile = document.getElementById("btn-mobile");
-navClass?.addEventListener("click", menuActive);
-function menuActive(event) {
-  if (btnMobile instanceof HTMLElement && navClass) {
-    // Ativo
-    if (navClass.classList.contains("active")) {
-      navClass.classList.remove("active");
-      btnMobile.setAttribute("aria-expanded", "false");
-      btnMobile.setAttribute("aria-label", "Abrir Menu");
-    }
-    // Inativo
-    else {
-      navClass.classList.add("active");
-      btnMobile.setAttribute("aria-expanded", "true");
-      btnMobile.setAttribute("aria-label", "Fechar Menu");
-    }
-  }
-  console.log(event);
+// Com generic
+function firstFive(data) {
+    return data.slice(0, 5);
 }
-// Solução Origamid
-// const navClass = document.getElementById("nav");
-// const btnMobile = event.currentTarget;
-// function toggleMenu(event: PointerEvent) {
-//   if (btnMobile instanceof HTMLElement && navClass) {
-//     const active = nav.classList.contains('active')
-//     if (active) {
-//       navClass.classList.remove("active");
-//       btnMobile.setAttribute("aria-expanded", "false");
-//       btnMobile.setAttribute("aria-label", "Abrir Menu");
-//     }
-//     // Inativo
-//     else {
-//       navClass.classList.add("active");
-//       btnMobile.setAttribute("aria-expanded", "true");
-//       btnMobile.setAttribute("aria-label", "Fechar Menu");
-//     }
-//   }
-//   console.log(event);
-// }
-// navClass?.addEventListener("click", toggleMenu);
+console.log(firstFive(numeros));
+console.log(firstFive(frutas));
+// Exemplo 3
+function notNull(arg) {
+    if (arg !== null)
+        return arg;
+    else
+        return null;
+}
+console.log(notNull(200)?.toFixed());
+console.log(notNull("André")?.toLowerCase());
+// Exemplo 4
+function tipoDado(a) {
+    const resultado = {
+        dado: a,
+        tipo: typeof a,
+    };
+    console.log(resultado);
+    return resultado;
+}
+tipoDado(true);
