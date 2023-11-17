b3b42f4d6a633bdf9e52092582deee41b8eaa9b1
Ítalo Henrique <italosantosdick@hotmail.com>
2023-11-16 23:08:57 -0300

    Type Guard e Control Flow

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 "use strict";
-// function somar(a: number, b: number, c?: number): number {
-//   return a + b + (c ? c : 0);
+// Type guard -> eu garanto através de uma verificação se a propriedade existe ou não.
+// function typeGuard(value: any) {
+//   if (typeof value === "string") {
+//     return value.toLowerCase();
+//   }
+//   if (typeof value === "number") {
+//     return value.toFixed();
+//   }
+//   if (value instanceof HTMLElement) {
+//     return value.innerText;
+//   }
 // }
-// somar(3, 4);
-// somar(3, 4, 1);
-function arredondar(valor) {
-    if (typeof valor === "number") {
-        return Math.ceil(valor);
-    }
-    else {
-        return Math.ceil(Number(valor)).toString();
+// typeGuard("Origamid");
+// typeGuard(200);
+// typeGuard(document.body);
+const obj = {
+    nome: "Origamid",
+};
+if ("nome" in obj) {
+    console.log("Sim");
+}
+async function fetchProduto() {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const json = await response.json();
+    handleProduto(json);
+}
+// O operador in verifica se o objeto possui uma propriedade com o mesmo nome da string comparada "propriedade" in obj.
+function handleProduto(data) {
+    if ("preco" in data) {
+        document.body.innerHTML += `
+      <p>Nome: ${data.nome}</p>
+      <p>Preço: R$ ${data.preco + 100}</p>
+    `;
     }
 }
-console.log(arredondar(200.45));
-console.log(arredondar("300.45"));
+fetchProduto();
