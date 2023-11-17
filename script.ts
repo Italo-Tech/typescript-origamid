32b0510b0b7650bc96753474b86567800ce102f6
Italo <italo@appbee.online>
2023-11-16 23:13:15 -0300

    Unknown

--- script.ts
+++ script.ts
@@ -1,48 +1,18 @@
-// Type guard -> eu garanto através de uma verificação se a propriedade existe ou não.
+// Unknown
+// Indica que não sabemos o tipo de dados que pode ser passado. Diferente do any, o unknown só irá permitir o uso de métodos quando a Type Safety estiver garantida.
 
-// function typeGuard(value: any) {
-//   if (typeof value === "string") {
-//     return value.toLowerCase();
-//   }
-//   if (typeof value === "number") {
-//     return value.toFixed();
-//   }
-//   if (value instanceof HTMLElement) {
-//     return value.innerText;
-//   }
-// }
-
-// typeGuard("Origamid");
-// typeGuard(200);
-// typeGuard(document.body);
-
-const obj = {
-  nome: "Origamid",
-};
-
-if ("nome" in obj) {
-  console.log("Sim");
-}
-
-interface Produto {
-  nome: string;
-  preco: number;
-}
-
-async function fetchProduto() {
-  const response = await fetch("https://api.origamid.dev/json/notebook.json");
-  const json = await response.json();
-  handleProduto(json);
-}
-
-// O operador in verifica se o objeto possui uma propriedade com o mesmo nome da string comparada "propriedade" in obj.
-function handleProduto(data: Produto) {
-  if ("preco" in data) {
-    document.body.innerHTML += `
-      <p>Nome: ${data.nome}</p>
-      <p>Preço: R$ ${data.preco + 100}</p>
-    `;
+function typeGuard(value: unknown) {
+  if (typeof value === "string") {
+    return value.toLowerCase();
+  }
+  if (typeof value === "number") {
+    return value.toFixed();
+  }
+  if (value instanceof HTMLElement) {
+    return value.innerText;
   }
 }
 
-fetchProduto();
+typeGuard("Origamid");
+typeGuard(200);
+typeGuard(document.body);
