6696d0f6f070861f5941846c74c67e1d69f322b9
Italo <italo@appbee.online>
2023-11-16 23:08:57 -0300

    Type Guard e Control Flow

--- script.ts
+++ script.ts
@@ -1,85 +1,48 @@
-// function somar(a: number, b: number, c?: number): number {
-//   return a + b + (c ? c : 0);
-// }
-// somar(3, 4);
-// somar(3, 4, 1);
+// Type guard -> eu garanto através de uma verificação se a propriedade existe ou não.
 
-// const subtrair = (a: number, b: number): number => a - b;
-
-// subtrair(10, 2);
-
-// type Callback = (event: MouseEvent) => void;
-
-// void
-// function pintarTela(cor: string) {
-//   document.body.style.background = cor;
-// }
-
-// pintarTela('black');
-
-// // Erro, void não pode ser verificado
-// if (pintarTela('black')) {
-// }
-
-// const btn = document.querySelector('button');
-
-// // Erro, void não pode ser verificado
-// if (btn && btn.click()) {
-// }
-
-// // Se a função tiver qualquer tipo de retorno,
-// // ela não terá mais o void como uma opção e sim o undefined
-// function isString(value: any) {
-//   if (typeof value === 'string') {
-//     return true;
+// function typeGuard(value: any) {
+//   if (typeof value === "string") {
+//     return value.toLowerCase();
+//   }
+//   if (typeof value === "number") {
+//     return value.toFixed();
+//   }
+//   if (value instanceof HTMLElement) {
+//     return value.innerText;
 //   }
 // }
 
-// if (isString('teste')) {
-//   console.log('É string');
-// }
+// typeGuard("Origamid");
+// typeGuard(200);
+// typeGuard(document.body);
 
-// never
-// function abortar(mensagem: string): never {
-//   throw new Error(mensagem);
-// }
+const obj = {
+  nome: "Origamid",
+};
 
-// abortar("Um erro ocorreu");
-// console.log("Tente novamente");
+if ("nome" in obj) {
+  console.log("Sim");
+}
 
-// método
-// interface Quadrado {
-//   lado: number;
-//   perimetro(lado: number): number;
-// }
+interface Produto {
+  nome: string;
+  preco: number;
+}
 
-// function calcular(forma: Quadrado) {}
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
 
-// Overload - O Overload deve ser compatível com a função original.
-// function normalizar(valor: string): string;
-// function normalizar(valor: string[]): string[];
-// function normalizar(valor: string | string[]): string | string[] {
-//   if (typeof valor === "string") {
-//     return valor.trim().toLowerCase();
-//   } else {
-//     return valor.map((item) => item.trim().toLowerCase());
-//   }
-// }
-// console.log(normalizar(" Produto "));
-// console.log(normalizar(["Banana ", " UVA"]));
-
-// Crie uma função que arredonda um valor passado para cima.
-// A função pode receber string ou number.
-// A função deve retornar o mesmo tipo que ela receber.
-function arredondar(valor: string): string;
-function arredondar(valor: number): number;
-function arredondar(valor: string | number): string | number {
-  if (typeof valor === "number") {
-    return Math.ceil(valor);
-  } else {
-    return Math.ceil(Number(valor)).toString();
+// O operador in verifica se o objeto possui uma propriedade com o mesmo nome da string comparada "propriedade" in obj.
+function handleProduto(data: Produto) {
+  if ("preco" in data) {
+    document.body.innerHTML += `
+      <p>Nome: ${data.nome}</p>
+      <p>Preço: R$ ${data.preco + 100}</p>
+    `;
   }
 }
 
-console.log(arredondar(200.45));
-console.log(arredondar("300.45"));
+fetchProduto();
