6891da2b7de65dedeccc8ea08a506e682b751d4a
Ítalo Henrique <italosantosdick@hotmail.com>
2023-11-20 18:41:53 -0300

    Type Guard e Type Predicate

--- script.js
+++ script.js
@@ -1,37 +1,88 @@
-"use strict";
-// Type guard -> eu garanto através de uma verificação se a propriedade existe ou não.
-// function typeGuard(value: any) {
-//   if (typeof value === "string") {
-//     return value.toLowerCase();
-//   }
-//   if (typeof value === "number") {
-//     return value.toFixed();
-//   }
-//   if (value instanceof HTMLElement) {
-//     return value.innerText;
-//   }
-// }
-// typeGuard("Origamid");
-// typeGuard(200);
-// typeGuard(document.body);
-const obj = {
-    nome: "Origamid",
-};
-if ("nome" in obj) {
-    console.log("Sim");
+// Uma array não pode ser verificada com o typeof pois a mesma é um object. Podemos verificar se o dado é instanceof Array ou podemos usar a função Array.isArray().
+async function fetchCursos() {
+    const response = await fetch("https://api.origamid.dev/json/cursos.json");
+    const json = await response.json();
+    handleCursos(json);
 }
+fetchCursos();
+function handleCursos(data) {
+    if (data instanceof Array) {
+        console.log("É instância de Array");
+    }
+    if (Array.isArray(data)) {
+        console.log("É array");
+    }
+}
+// Type Predicate
+function isString(value) {
+    return typeof value === "string";
+}
+function handleData(data) {
+    if (isString(data)) {
+        data.toUpperCase();
+    }
+}
+handleData("Origamid");
+handleData(200);
+// Objetos
+// O Type Predicate pode ser especialmente utilizado para criarmos Type Guards para objetos específicos e garantirmos a Type Safety (segurança) do projeto.
 async function fetchProduto() {
     const response = await fetch("https://api.origamid.dev/json/notebook.json");
     const json = await response.json();
     handleProduto(json);
 }
-// O operador in verifica se o objeto possui uma propriedade com o mesmo nome da string comparada "propriedade" in obj.
-function handleProduto(data) {
-    if ("preco" in data) {
-        document.body.innerHTML += `
-      <p>Nome: ${data.nome}</p>
-      <p>Preço: R$ ${data.preco + 100}</p>
-    `;
+fetchProduto();
+function isProduto(value) {
+    if (value &&
+        typeof value === "object" &&
+        "nome" in value &&
+        "preco" in value) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
-fetchProduto();
+function handleProduto(data) {
+    if (isProduto(data)) {
+        console.log(data);
+    }
+}
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+async function fetchMyCurse() {
+    const response = await fetch("https://api.origamid.dev/json/cursos.json");
+    const json = await response.json();
+    handleMyCurse(json);
+}
+fetchMyCurse();
+// Minha Type Guard com Type Predicate
+function isCurse(value) {
+    if (value &&
+        typeof value === "object" &&
+        "nome" in value &&
+        "horas" in value &&
+        "tags" in value) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function handleMyCurse(data) {
+    if (Array.isArray(data)) {
+        data.filter(isCurse).forEach((item) => {
+            document.body.innerHTML += `
+        <div>
+          <h2>${item.nome}</h2>
+          <p>${item.horas}</p>
+          <p>${item.tags.join(", ")}</p>
+        </div>
+      `;
+        });
+    }
+}
+export {};
--- script.ts
+++ script.ts
@@ -1,18 +1,119 @@
-// Unknown
-// Indica que não sabemos o tipo de dados que pode ser passado. Diferente do any, o unknown só irá permitir o uso de métodos quando a Type Safety estiver garantida.
+// Uma array não pode ser verificada com o typeof pois a mesma é um object. Podemos verificar se o dado é instanceof Array ou podemos usar a função Array.isArray().
 
-function typeGuard(value: unknown) {
-  if (typeof value === "string") {
-    return value.toLowerCase();
+import { forEach } from "lodash";
+
+async function fetchCursos() {
+  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+  const json = await response.json();
+  handleCursos(json);
+}
+fetchCursos();
+
+function handleCursos(data: unknown) {
+  if (data instanceof Array) {
+    console.log("É instância de Array");
   }
-  if (typeof value === "number") {
-    return value.toFixed();
-  }
-  if (value instanceof HTMLElement) {
-    return value.innerText;
+  if (Array.isArray(data)) {
+    console.log("É array");
   }
 }
 
-typeGuard("Origamid");
-typeGuard(200);
-typeGuard(document.body);
+// Type Predicate
+function isString(value: unknown): value is string {
+  return typeof value === "string";
+}
+
+function handleData(data: unknown) {
+  if (isString(data)) {
+    data.toUpperCase();
+  }
+}
+
+handleData("Origamid");
+handleData(200);
+
+// Objetos
+// O Type Predicate pode ser especialmente utilizado para criarmos Type Guards para objetos específicos e garantirmos a Type Safety (segurança) do projeto.
+
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
+fetchProduto();
+
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+  if (
+    value &&
+    typeof value === "object" &&
+    "nome" in value &&
+    "preco" in value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleProduto(data: unknown) {
+  if (isProduto(data)) {
+    console.log(data);
+  }
+}
+
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+
+async function fetchMyCurse() {
+  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+  const json = await response.json();
+  handleMyCurse(json);
+}
+fetchMyCurse();
+
+interface Curse {
+  nome: string;
+  aulas: number;
+  gratuito: boolean;
+  idAulas: number;
+  horas: number;
+  nivel: "inciante" | "avancado";
+  tags: string[];
+}
+
+// Minha Type Guard com Type Predicate
+function isCurse(value: unknown): value is Curse {
+  if (
+    value &&
+    typeof value === "object" &&
+    "nome" in value &&
+    "horas" in value &&
+    "tags" in value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleMyCurse(data: unknown) {
+  if (Array.isArray(data)) {
+    data.filter(isCurse).forEach((item) => {
+      document.body.innerHTML += `
+        <div>
+          <h2>${item.nome}</h2>
+          <p>${item.horas}</p>
+          <p>${item.tags.join(", ")}</p>
+        </div>
+      `;
+    });
+  }
+}
